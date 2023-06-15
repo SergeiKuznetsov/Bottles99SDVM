ce26f9a6732855a8dd77a2ddab45d85961b859c4
Maksim <maksim.meleshkin@eikon.group>
2023-06-14 20:08:47 -0700

    new changes in test added

--- src/test/java/tests/MainPageTest.java
+++ src/test/java/tests/MainPageTest.java
@@ -14,5 +14,7 @@ public class MainPageTest extends BaseTest {
         getDriver().navigate().to("https://redrover.school/");
         getDriver().navigate().to("https://redrover.school/");
         getDriver().navigate().to("https://redrover.school/");
+
+
     }
 }
