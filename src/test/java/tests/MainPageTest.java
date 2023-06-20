33509efab8266fe7767bddf8d49d32fdfc085716
Maksim Meleshkin <116033547+Maksim2404@users.noreply.github.com>
2023-06-19 17:07:44 -0700

    RF (#8)
    * new changes in test added
    
    * new changes in test added
    
    * new changes added
    
    * new changes in test added
    
    * new changes in test added
    
    * new changes in test added
    
    * new changes in test added
    
    * new changes in test added
    
    * new changes added
    
    * new changes added
    
    * new changes added
    
    * new changes added
    
    * new changes added
    
    ---------
    
    Co-authored-by: Maksim <maksim.meleshkin@eikon.group>

--- src/test/java/tests/MainPageTest.java
+++ src/test/java/tests/MainPageTest.java
@@ -6,11 +6,16 @@ import org.testng.annotations.Test;
 public class MainPageTest extends BaseTest {
 
     @Test
-    public void checkTheTitle() {
+    public void checkTheTitleTest() {
 
         openBaseURL();
         getDriver().getTitle();
         getDriver().navigate().to("https://www.google.com/");
         getDriver().navigate().to("https://www.ups.com/track?loc=en_US&requester=ST/trackdetails");
+    }
+    @Test
+    public void checkTheNewTest() {
+        openBaseURL();
+        getDriver().navigate().to("https://docs.github.com");
     }
 }
